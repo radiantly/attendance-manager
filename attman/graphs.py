48a3678d7c77846f0ce9a3a11ae4b741046fd4c2
radiantly <44368997+radiantly@users.noreply.github.com>
2020-11-08 09:45:21 +0530

    Generate graph

--- attman/graphs.py
+++ attman/graphs.py
@@ -1,10 +1,23 @@
+from attman.models import AttnNumbers
+
 from matplotlib import pyplot as plt
 from matplotlib import *
 
+
 def graph(dates, attended):
-    save_results_to = 'attman/static/graphs/'
+    save_results_to = "attman/static/graphs/"
     plt.plot(dates, attended)
-    plt.xlabel('Date')  
-    plt.ylabel('Total Students Attended')
-    plt.title('Analysis')
-    plt.savefig(save_results_to + 'Analysis.png')
+    plt.xlabel("Date")
+    plt.ylabel("Total Students Attended")
+    plt.title("Analysis")
+    plt.savefig(save_results_to + "Analysis.png")
+
+
+def generateGraph():
+    AttendanceNumbers = AttnNumbers.query.all()
+    a = []
+    d = []
+    for attn in AttendanceNumbers:
+        a.append(attn.attended)
+        d.append(attn.date.strftime("%d/%m/%y"))
+    graph(d, a)
